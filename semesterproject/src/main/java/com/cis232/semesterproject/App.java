eeaea5b218f8824b7eda7c49e02ec82221afc636
Dylan <Dylan@192.168.131.222>
2015-11-05 18:55:48 -0500

    Signed-off-by: Dylan <Dylan@192.168.131.222>

---
+++ semesterproject/src/main/java/com/cis232/semesterproject/App.java
@@ -0,0 +1,13 @@
+package com.cis232.semesterproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
